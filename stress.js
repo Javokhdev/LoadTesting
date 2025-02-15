4e84285c7dbe9943fbd3ed28b2639eb2ebe4a1fa
Javohir Khasanov <javohdev@gmail.com>
2025-02-16 01:11:18 +0500

    new testing

--- stress.js
+++ stress.js
@@ -1,18 +1,21 @@
-import http from 'k6/http';
-import { check } from 'k6';
+import http, { url } from 'k6/http';
+import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-      { duration: '1m', target: 10 }, // Gradually increase to 10 users
-      { duration: '2m', target: 50 }, // Sustain 50 users
-      { duration: '1m', target: 0 }, // Ramp down to 0
+      { duration: '5s', target: 5000 }, // Gradually increase to 10 users
+      { duration: '30s', target: 5000 }, // Sustain 50 users
+      { duration: '5s', target: 0 }, // Ramp down to 0
     ],
   };
-  
   export default function () {
-    let res = http.get('http://localhost:8080/v1/user/list?page=1&limit=100');
-    check(res, {
+    const headers = {
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwbGF0Zm9ybSI6IndlYiIsInNlc3Npb25faWQiOiIwNjZhYTQyMy1iZDVjLTQ5ODQtOGJjMy0wMTFiNTE4ZWY5YTUiLCJzdWIiOiIwMzJmNmQ5Yy01Yjc1LTQyMTYtYTA4OC1kN2U3MGRlNTMyYWYiLCJ1c2VyX3JvbGUiOiJ1c2VyIiwidXNlcl90eXBlIjoidXNlciJ9.ugIGOX_o4t2qvEFebb41ZPK_VnxNdAtu14pzX2iCVN0"
+    }
+
+    let res = http.get( 'http://91.206.179.231:8080/v1/course/list?page=1&limit=10&language=uz', { headers: headers });
+check(res, {
       'status is 200 or 201': (r) => r.status === 200 || r.status === 201,
     });
+    sleep(1)
   }
-  
